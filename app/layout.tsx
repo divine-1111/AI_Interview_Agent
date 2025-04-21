7b10208d6ce2f0d253090b71d555a5dc7e5b51b9
divine-1111 <bt21ece025@nituk.ac.in>
2025-04-21 18:57:14 +0530

    dependencies installed

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "MockMate",
+  description: "An AI-powered platform for preparing for mock interviews.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="Dark">
+      <body
+        className={`${monaSans.className} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
